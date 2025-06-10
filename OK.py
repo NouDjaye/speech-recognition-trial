cf084457b43df6cbaf75281a8993d13955d92b02
NouDjaye <noudjayejunior2002@gmail.com>
2025-06-10 04:00:41 +0200

    Add files via upload

---
+++ OK.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Test Frontend - No Drama")
+
+if st.button("Clique-moi"):
+    st.write("Bouton cliqué, tout roule 🚀")
+
+st.text("Si ce code passe sans erreur, c’est que le front est clean.")
